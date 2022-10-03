74ec7caf31f5463b234f8e1b2df8ff594523677e
Juan Martín Núñez Aréchiga <jmartin.nuar@gmail.com>
2022-10-03 15:05:37 -0500

    Ejercicio 1 resuelto

---
+++ codigo-otros-1-main/assets/js/app.js
@@ -0,0 +1,28 @@
+//Se unifica toda la URL en una variable para mejor manejo
+
+const usersEndpoint = 'https://api.github.com/users';
+
+/* Se cambia el nombre de las variables por uno más comprensivo y se cambia el método de
+traerlos del HTML */
+
+const nombre = document.getElementById('name');
+const blog = document.getElementById('blog');
+// Se borra location porque no se usará
+
+// Se unen las dos funciones en una sola
+function displayUser(username) {
+  fetch(`${usersEndpoint}/${username}`)
+  .then(data => data.json()) // Se convierten los datos a json
+  .then(data =>{
+    nombre.innerHTML = data.name;
+    blog.innerHTML = data.blog;
+    blog.setAttribute("href", data.blog);
+    //
+  }).catch(error => {
+    console.log('OH NO');
+    console.log(error);
+    nombre.textContent = `Algo salió mal: ${error}`
+  });
+}
+
+displayUser('stolinski');
---
+++ codigo-otros-2-main/formulario.js
@@ -0,0 +1,96 @@
+// Se nombran variable con nombres más específicos y por medio de la id
+
+let campoNombre = document.getElementById("name");
+let campoEdad = document.getElementById("age");
+let campoNacionalidad = document.getElementById("nacionality");
+
+var formulario = document.querySelector("#form")
+
+// Se cambia el evento por un event listener
+formulario.addEventListener("submit", function (event) {
+
+  event.preventDefault();
+  
+  let nombre = campoNombre.value;
+  let edad = campoEdad.value;
+
+  let i = campoNacionalidad.selectedIndex
+  var nacionalidad = campoNacionalidad.options[i].value
+  console.log(nombre, edad)
+  console.log(nacionalidad)
+
+  if (nombre.length === 0) {
+    campoNombre.classList.add("error");
+  }
+  else if (edad < 18 || edad > 120) {
+    campoEdad.classList.add("error");
+  } else{
+  agregarInvitado(nombre, edad, nacionalidad);
+  }
+})
+
+var botonBorrar = document.createElement("button")
+botonBorrar.textContent = "Eliminar invitado"
+botonBorrar.id = "boton-borrar"
+var corteLinea = document.createElement("br")
+document.body.appendChild(corteLinea)
+document.body.appendChild(botonBorrar);
+
+function agregarInvitado(nombre, edad, nacionalidad) {
+
+  if (nacionalidad === "ar") {
+    nacionalidad = "Argentina"
+  }
+  else if (nacionalidad === "mx") {
+    nacionalidad = "Mexicana"
+  }
+  else if (nacionalidad === "vnzl") {
+    nacionalidad = "Venezolana"
+  }
+  else if (nacionalidad === "per") {
+    nacionalidad = "Peruana"
+  }
+
+var lista = document.getElementById("lista-de-invitados")
+
+var elementoLista = document.createElement("div")
+elementoLista.classList.added("elemento-lista")
+lista.appendChild(elementoLista)
+
+var spanNombre = document.createElement("span")
+var inputNombre = document.createElement("input")
+var espacio = document.createElement("br")
+spanNombre.textContent = "Nombre: "
+inputNombre.value = nombre 
+elementoLista.appendChild(spanNombre)
+elementoLista.appendChild(inputNombre)
+elementoLista.appendChild(espacio)
+
+function crearElemento(descripcion, valor) {
+var spanNombre = document.createElement("span")
+var inputNombre = document.createElement("input")
+var espacio = document.createElement("br")
+spanNombre.textContent = descripcion + ": "
+inputNombre.value = valor 
+elementoLista.appendChild(spanNombre)
+elementoLista.appendChild(inputNombre)
+elementoLista.appendChild(espacio)
+}
+
+crearElemento("Nombre", nombre)
+crearElemento("Edad", edad)
+crearElemento("Nacionalidad", nacionalidad)
+
+
+var botonBorrar = document.createElement("button")
+botonBorrar.textContent = "Eliminar invitado"
+botonBorrar.id = "boton-borrar"
+var corteLinea = document.createElement("br")
+elementoLista.appendChild(corteLinea)
+elementoLista.appendChild(botonBorrar);
+
+ botonBorrar.onclick = function() {
+// this.parentNode.style.display = 'none';
+botonBorrar.parentNode.remove()
+  }
+}
